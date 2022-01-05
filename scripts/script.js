757adc3a39f97cfc4ba525cb29042e08e2eefbee
EricEOL <lopes.eric051@gmail.com>
2022-01-05 09:39:26 -0300

    Section de o que dizem sobre nós e a lógica do slider dos depoimentos e botões

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,96 @@
+/* TESTEMONIALS SLIDER */
+
+window.addEventListener('load', start);
+const interval = 12000;
+
+function start() {
+
+  createTestemonialsSliderButtons(testemonialButtonSlider);
+
+  setInterval(() => {
+    autoNextTestemonial();
+  }, interval)
+}
+
+const testemonials = document.querySelectorAll('.testemonial');
+let maxTestemonialsLength = testemonials.length;
+let currentTestemonial = 0;
+let currentTestemonialButton = 0;
+
+function autoNextTestemonial() {
+
+  testemonials[currentTestemonial].classList.remove('visible');
+
+  currentTestemonial++;
+
+  if (currentTestemonial >= maxTestemonialsLength) {
+    currentTestemonial = 0;
+  }
+
+  testemonials[currentTestemonial].classList.add('visible');
+}
+
+function handlerNextTestemonial(index) {
+  testemonials[currentTestemonial].classList.remove('visible');
+  testemonials[index].classList.add('visible');
+  currentTestemonial = index;
+}
+
+/* BUTTONS TESTEMONIALS SLIDER */
+
+function createTestemonialsSliderButtons(callback) {
+  const sliderTestemonialsButtons = document.querySelector('.slider-testemonials-buttons');
+
+  testemonials.forEach((element, index) => {
+    const sliderButton = sliderTestemonialsButtons.appendChild(document.createElement('button'));
+    sliderButton.addEventListener('click', () => {
+      handlerNextTestemonial(index);
+      handlerNextTestemonialButton(index);
+    })
+  })
+  const firstButton = sliderTestemonialsButtons.firstChild.nextSibling;
+  firstButton.classList.add('current')
+
+  callback();
+}
+
+function testemonialButtonSlider() {
+  const buttons = document.querySelectorAll('.slider-testemonials-buttons button');
+
+  setInterval(() => {
+    buttons[currentTestemonialButton].classList.remove('current');
+
+    currentTestemonialButton++;
+
+    if (currentTestemonialButton >= maxTestemonialsLength) {
+      currentTestemonialButton = 0;
+    }
+
+    buttons[currentTestemonialButton].classList.add('current');
+  }, interval)
+}
+
+function handlerNextTestemonialButton(index) {
+  const buttons = document.querySelectorAll('.slider-testemonials-buttons button');
+
+  buttons[currentTestemonialButton].classList.remove('current');
+  buttons[index].classList.add('current');
+  currentTestemonialButton = index;
+}
+
+/* HEADER */
+
 const header = document.querySelector('header');
 
-/* window.addEventListener('touchmove', (event) => {
-  if(event.path[5].pageYOffset > 100) {
-    header.classList.add('headerScrolled');
-  } else {
-    header.classList.remove('headerScrolled');
-  }
-}) */
-
 window.addEventListener('scroll', (event) => {
-  if(event.path[1].scrollY > 100) {
+  if (event.path[1].scrollY > 100) {
     header.classList.add('headerScrolled');
   } else {
     header.classList.remove('headerScrolled');
   }
 })
 
+/* MENU FOR MOBILE DEVICES */
 
 const menuMobileBtn = document.querySelector(".menu-hamburger");
 
